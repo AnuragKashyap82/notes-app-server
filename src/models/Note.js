8bb8c22fa68dfa152c509f59bcfba7044032a5e5
Anurag Kashyap <anuragkashyap815353@gmail.com>
2023-08-11 20:56:12 +0530

    notes app server

---
+++ src/models/Note.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+//Step 1: Create Schema
+const noteSchema = mongoose.Schema({
+   id: {
+    type: String,
+    unique: true,
+    required: true
+   },
+   userid: {
+    type: String,
+    required: true,
+   },
+   title: {
+    type: String,
+   },
+   dateAdded: {
+    type: Date,
+    default: Date.now,
+   },
+});
+
+//Step 2: Creating model using Schema and export the schema.
+
+module.exports = mongoose.model("Note", noteSchema);
---
+++ src/server.js
@@ -0,0 +1,30 @@
+//Initialization
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+const Note = require('./models/Note');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://anuragkashyap82:anurag1234@cluster0.sgm5njq.mongodb.net/notesdb").then(function(){
+
+app.get("/", function(req, res){
+    const response = { statuscode: res.statusCode, message: "API Works!!!"};
+    res.json(response);
+ });
+
+ const noteRouter = require("./routes/Note");
+    app.use("/notes", noteRouter);
+});
+
+
+
+
+ // Starting the server on a port
+ const PORT = process.env.PORT || 5000;
+app.listen(PORT, function(){
+    console.log("Server started at port 5000");
+});
